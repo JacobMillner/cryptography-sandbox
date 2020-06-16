37d126ebd590442a90ba3096d29aac515b60b5cb
Jacob Millner <jacob.millner@outlook.com>
2020-06-16 17:25:59 -0400

    playing around in go

--- vigenere-cipher/vigenere-cipher.go
+++ vigenere-cipher/vigenere-cipher.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("TODO")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	text, _ := reader.ReadString('\n')
+	fmt.Println(text)
 }
