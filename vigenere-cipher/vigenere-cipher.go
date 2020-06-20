149766e306739c1e8f69a0a0233a07b27a5c309e
Jacob Millner <jacob.millner@outlook.com>
2020-06-20 12:02:56 -0400

    learning some go

--- vigenere-cipher/vigenere-cipher.go
+++ vigenere-cipher/vigenere-cipher.go
@@ -3,12 +3,31 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter text: ")
-	text, _ := reader.ReadString('\n')
-	fmt.Println(text)
+	fmt.Print("Encrypt (Enter 1)? Decrypt (Enter 2): ")
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	input = strings.TrimSpace(input)
+	num, err := strconv.ParseInt(input, 0, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if num == 1 {
+		fmt.Println("You chose Encrypt!")
+	} else if num == 2 {
+		fmt.Println("You chose Decrypt!")
+	} else {
+		fmt.Println("Incorrect choice!")
+	}
+	fmt.Println(input)
 }
